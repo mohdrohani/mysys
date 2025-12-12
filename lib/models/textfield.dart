8e5d73b2aff8a6a74e468262985ae881507ee0e0
mohdrohani <mohdalrawhani@gmail.com>
2025-12-12 08:48:50 +0300

    TEXTFIELD ALIGNMENT AND BORDER COLOR CHANGES

--- lib/models/textfield.dart
+++ lib/models/textfield.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 class InputField extends StatelessWidget{
   final String inputTextHolder;
+  final Color focusedBorderColor;
 
   const InputField({
     super.key,
     required this.inputTextHolder,    
+    this.focusedBorderColor=Colors.white,
   });
   
   @override
   Widget build(BuildContext context) 
   {
-    return TextField(        
-      
+    return TextField(
+      textAlignVertical:TextAlignVertical.center,
+      textAlign:TextAlign.center,
       style: TextStyle(fontSize: 12,color: Colors.white),
       decoration: InputDecoration(
-        border: OutlineInputBorder(),        
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey, width: 1.5),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color:focusedBorderColor, width: 2.5),
+          borderRadius: BorderRadius.circular(8.0),
+        ),     
+        border: OutlineInputBorder(),
         hintText: inputTextHolder, 
-        contentPadding: EdgeInsets.all(10),     
+        hintStyle: TextStyle(
+          color: Colors.grey, // set your color here          
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 0,vertical: 5),     
         isDense: true, 
       ),
     );
